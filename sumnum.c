16416c0e2fa60373249777b890d22f6672e183cd
shivaditya349 <76464806+shivaditya349@users.noreply.github.com>
2021-03-17 15:38:39 +0530

    sumnum

---
+++ sumnum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int num1,num2,sum;
+	printf("enter the first number\n");
+	scanf("%d",&num1);
+	printf("enter the second number\n");
+	scanf("%d",&num2);
+	printf("%d sum",num1+num2);
+	return 0;
+}
