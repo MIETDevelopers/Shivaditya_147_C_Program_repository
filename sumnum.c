b8b86a4e2bc9a7e1f4f76c383fb490ac83b8cf8f
shivaditya349 <76464806+shivaditya349@users.noreply.github.com>
2021-03-18 20:57:52 +0530

    sumnum.c

--- sumnum.c
+++ sumnum.c
@@ -1,10 +1,16 @@
-#include<stdio.h>
+//Author name: Shivaditya sharma 
+//creation date: 17-03-2021
+// purpose:sum of the two numbers
+#include<stdio.h> //Preprocessor directive to include input output function header file
+//start of the main body function
 int main(){
-	int num1,num2,sum;
-	printf("enter the first number\n");
-	scanf("%d",&num1);
-	printf("enter the second number\n");
-	scanf("%d",&num2);
-	printf("%d sum",num1+num2);
-	return 0;
-}
+	int n1,n2,s ;// Declaring variables of int data type
+	printf("enter the first number\n");//printf function calling to print the value of first number
+	scanf("%d",&n1);//scanf function calling to read the user input
+	printf("enter the second function\n");//printf function calling to print the value of second number
+	scanf("%d",&n2);//scanf funtion calling to read the user input
+	printf("%d sum",n1+n2);//printf funtion calling to print the result
+
+	return 0;//return statement
+
+}//End of main funtion body
