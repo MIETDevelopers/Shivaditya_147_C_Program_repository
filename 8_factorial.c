296ae5ba58021626658c72710d6f3d1386477d24
shivaditya349 <76464806+shivaditya349@users.noreply.github.com>
2021-03-18 20:37:51 +0530

    8_factorial

--- 8_factorial.c
+++ 8_factorial.c
@@ -1,12 +1,16 @@
-#include<stdio.h>
+//Author Name: Shivaditya sharma
+//Creation Date: 17 March 2021
+//Purpose: To find the Factorial of a Number using for loop 
+#include<stdio.h>//Preprocessor directive to include input output function header file
+//Start of the main body
 int main(){
-	int n,i,f=1;
-	printf("Enter a Number : ");
-	scanf("%d",&n);
-	for (i=1; i<=n; i++)
+	int n,i,f=1;//Declaring variables of integer data type
+	printf("Enter a Number : ");//Printf function calling to print a Number
+	scanf("%d",&n);//Scanf function calling to read user input
+	for (i=1; i<=n; i++)//Start of for loop
 	{
-		f=f*i;
+		f=f*i;//factorial formula
 	}
-	printf("%d",f);
-	return 0;
-}
+	printf("%d",f);//Printf function calling to print factorial
+	return 0;//Return statement
+}//End of the main function body
