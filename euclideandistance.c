4564378c20329da6c6c240f1a5d166f5e14c98d0
shivaditya349 <76464806+shivaditya349@users.noreply.github.com>
2021-03-18 20:44:59 +0530

    euclideandistance

--- euclideandistance.c
+++ euclideandistance.c
@@ -1,12 +1,18 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-	float x1,y1,x2,y2,distance;
-	printf("enter point1 (x1,y1)\n");
-	scanf("%f%f",&x1,&y1);
-	printf("enter point2 (x2,y2)\n");
-	scanf("%f%f",&x2,&y2);
-	distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-	printf("distance between (%f,%f)and(%f,%f)is%f\n",x1,y1,x2,y2,distance);
-	return 0;
-}
+//Author name: Shivaditya Sharma
+//Date:17-03-2021
+//purpose:Find Euclidean distance between two points in a plane
+#include<stdio.h> //Preprocessor directive to include input output function header file
+#include<math.h>//Preprocessor directive to include maths header file
+//start of the main body function 
+int main()
+{
+	float x1,y1,x2,y2,d;//declaring variables of float data type
+	printf("enter point1 (x1,y1)\n");//printf function calling to print the values of point x1,y1
+	scanf("%f%f",&x1,&y1);//scanf function calling to read the values of point x1,y1
+	printf("enter points2 (x2,y2)\n");//printf function calling to print the values of points x2,y2
+	scanf("%f%f",&x2,&y2);//scanf function calling to read the values of point x2,y2
+	d=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));//apply the distance formula
+	printf("distance betweem these two points is %f \n" ,x1,x2,y1,y2,d);//printf funtion calling to print the distance between the two points 
+
+	return 0;//return statement 
+}//end of main body functions
