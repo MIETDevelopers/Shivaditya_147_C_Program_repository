9006282ae8fd44577a3181c45649a93d454dff6b
shivaditya349 <76464806+shivaditya349@users.noreply.github.com>
2021-04-04 20:20:55 +0530

    30_ReadWriteFile

--- 30_ReadWriteFile.c
+++ 30_ReadWriteFile.c
@@ -1,4 +1,5 @@
-//A C program to Program for reading and writing text files.				© Ishav Verma 21/March/2021
+//Author name : Shivaditya Sharma
+//A C program to Program for reading and writing text files.			
 #include <stdio.h>
 #include <stdlib.h>
 struct s {
