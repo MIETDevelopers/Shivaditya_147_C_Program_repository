45354bd3f4153124e96c3642423b5de2e142e7e2
shivaditya349 <76464806+shivaditya349@users.noreply.github.com>
2021-04-04 19:31:34 +0530

    21_PassString

--- 21_PassString.c
+++ 21_PassString.c
@@ -1,4 +1,5 @@
-//A C Program for passing string to a Function. 			© Ishav Verma 19/March/2021
+//Author name : Shivaditya Sharma
+//A C Program for passing string to a Function. 
 #include <stdio.h>
 void displayString(char str[]);
 
