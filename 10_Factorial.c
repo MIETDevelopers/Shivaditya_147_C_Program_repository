6b94fe4d01fc54e0540051a1836f9f49e37b3b9a
shivaditya349 <76464806+shivaditya349@users.noreply.github.com>
2021-04-04 19:08:45 +0530

    10_Factorial

--- 10_Factorial.c
+++ 10_Factorial.c
@@ -1,4 +1,5 @@
-//A C Program to calculate Factorial of a number.               © Ishav Verma 18/March/2021
+//Author name : Shivaditya Sharma 
+//A C Program to calculate Factorial of a number.        
 #include<stdio.h>  
 int main()    
 {    
