fb15eebf4584258fdc66a9c51cf0c1517087af2c
shivaditya349 <76464806+shivaditya349@users.noreply.github.com>
2021-04-04 20:28:39 +0530

    029_StudentArrStruct

--- 029_StudentArrStruct.c
+++ 029_StudentArrStruct.c
@@ -1,4 +1,5 @@
-//A C Program to store Array of structures for student marks for each student and passing it to function for various operations.          © Ishav Verma 20/March/2021
+//Author name : Shivaditya Sharma
+//A C Program to store Array of structures for student marks for each student and passing it to function for various operations.     
 #include <stdio.h>
 #include <string.h>
  
